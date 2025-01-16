5a1162f6157596ecf6dac4d69a2594ac5959a8b6
Shogo Matsumoto <shogo.matsumoto@avanade.com>
2025-01-16 15:39:44 +0900

    Fix bad request condition

--- Functions/Functions.Sample/Functions.Sample/HttpTriggerSample001.cs
+++ Functions/Functions.Sample/Functions.Sample/HttpTriggerSample001.cs
@@ -23,7 +23,7 @@ namespace Functions.Sample
             var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic? data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
-            return name != null ? new OkObjectResult($"Welcome to Azure Functions! {name}.") : new OkObjectResult($"Please pass a name on the query string or in the request Body");
+            return !string.IsNullOrEmpty(name) ? new OkObjectResult($"Welcome to Azure Functions! {name}.") : new BadRequestObjectResult($"Please pass a name on the query string or in the request Body");
         }
     }
 }
